16e28463745562ce8eecbfe275613529db72959b
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-10 14:12:59 +0200

    added comments and details

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
- const row = document.querySelector('.row');
+/* Traccia:
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero
+e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
 
+/* 
+Strumenti:
+for loop 
+document.createElement()
+append method
+classlist methods */
+
+const row = document.querySelector('.row');
 
 for(let i = 1; i < 106; i++){
     const box = document.createElement('div');
