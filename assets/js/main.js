9366ed4726e570455d9a97fd0ba4f5b20d8816d7
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-10 14:09:47 +0200

    edited style and  colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,13 @@ for(let i = 1; i < 106; i++){
     row.append(box);
     if (i % 5 == 0 && i % 3 == 0){
         box.append("FizzBuzz");
-        box.style.backgroundColor = "red";
+        box.style.backgroundColor = "var(--bg-fizzBuzz)";
     } else if (i % 3 == 0){
         box.append("Fizz");
-        box.style.backgroundColor = "blue"
+        box.style.backgroundColor = "var(--bg-fizz)"
     } else if (i % 5 == 0) {
         box.append("Buzz");
-        box.style.backgroundColor = "green"
+        box.style.backgroundColor = "var(--bg-Buzz)"
     } else {
         box.append(i);
     }
